0d4629638c11f80e05646dd412e6819cf938920f
CalebBarbaGarcia <caleb.barba@alumnos.udg.mx>
2020-10-12 17:07:25 -0500

    Se modifico programa Go3

--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import "fmt"
 
 func main() {
 	var s []int
-	var tamano int
+	var tamano uint
 	var aux int
 	var i int
+	var i1 uint
 	var resultado int
 
 	
-	fmt.Scan(&tamano)
-	i = 0
-	for i < tamano {
-		fmt.Scan(&aux)
+	fmt.Scanln(&tamano)
+	i1 = 0
+	
+	for i1 < tamano {
+		fmt.Scanln(&aux)
 		s = append(s,aux)
-		i = i + 1
+		i1 = i1 + 1
 	}
 
 	i = 0
