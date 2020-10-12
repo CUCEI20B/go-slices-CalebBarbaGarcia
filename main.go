0bc1d3c46ef8a21fcd05adc749942f68d4e41e3b
CalebBarbaGarcia <caleb.barba@alumnos.udg.mx>
2020-10-12 16:53:56 -0500

    Se hizo programa Go3

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var s []int
+	var tamano int
+	var aux int
+	var i int
+	var resultado int
+
+	
+	fmt.Scan(&tamano)
+	i = 0
+	for i < tamano {
+		fmt.Scan(&aux)
+		s = append(s,aux)
+		i = i + 1
+	}
+
+	i = 0
+
+	for i < len(s) {
+		resultado = resultado + s[i]
+		i = i + 1
+	}
+
+	println(resultado)
 }
